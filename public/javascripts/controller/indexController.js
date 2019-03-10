7ceeb4122ddd7d5fa592907450115b9af64b5bc0
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-03-11 02:49:08 +0300

    newUser name emit to server  and default position data

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,15 +1,29 @@
 app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=>{
     
-    const connectionOptions = {
-        reconnectionAttempts:3,
-        reconnectionDelay:600
+    $scope.init = () =>{                        //index.pug sayfa acıldıgında calısacak.index.pug içerisinde tanımlı dive ng-init="init()" ile bu kısmı tanımlıyoruz.
+        const username = prompt('Please enter username');   //prompt ile kullanıcıdan isim alacagız.
+
+        if(username)                    //eger prompta isim girilmiş ise initsocket fonks cagırarak kullanıcı adı  datasını socket.emit ile back ende gönderiyoruz.
+            initSocket(username);
+        else
+            return false;
     };
 
-    indexFactory.connectSocket('http://localhost:3000',connectionOptions)   //fonksiyon cagrıldıktan sonra connect saglanırsa socket.on('connect') resolve dönüyor ve then ile burda yakalıyoruz..
-    .then((socket)=>{                                   //başarılı oldugunda thene düştügünde --> indexFactoryde promisela  gelen socket datasını alıyoruz.
-        console.log('connect succesfull',socket);       //bağlantı yapan objeyi console a yazdırıyoruz.
-    }).catch((err)=>{
-        console.log(err);
-    });
+
+    function initSocket(username){
+
+        const connectionOptions = {
+            reconnectionAttempts:3,
+            reconnectionDelay:600
+        };
+    
+        indexFactory.connectSocket('http://localhost:3000',connectionOptions)   //fonksiyon cagrıldıktan sonra connect saglanırsa socket.on('connect') resolve dönüyor ve then ile burda yakalıyoruz..
+        .then((socket)=>{                                   //başarılı oldugunda thene düştügünde --> indexFactoryde promisela  gelen socket datasını alıyoruz.
+            socket.emit('newUser',{username:username})  //prompta girillen kullanıcı adını alarak newUser adlı emit ile back ende gönderiyoruz.   src içerisinde socketApi.js de karsılıyoruz.  
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+   
 
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -6,8 +6,25 @@ const socketApi={};
 
 socketApi.io=io;        // sunucunun basladıgı bin www da kullanabilmek için socketio constructorunu/ www da attach işlemi gerceklestirmem gerekiyor.
 
+const users = [];       //Baglanan her kullanıcı datalarını dizide tutmam gerekli.
+
 io.on('connection',(socket)=>{          //herhangi bir connection eventi oldugunda tetiklenecek method.
     console.log('a user connected');
+
+    socket.on('newUser',(data)=>{       //Client de (indexCntroller) prompt ile gelen emiti(data->username) karsıladık.
+        const defaultData = {
+            id: socket.id,
+            position: {
+                x:0,
+                y:0
+            }
+        }
+
+        const userData = Object.assign(data,defaultData);  //emitle gelen username datasını ve kullanıcı id ve position datalarını tek bir obje içinde birleştiriyoruz.
+        users.push(userData);
+        console.log(users);
+    });
+
 });
 
 module.exports = socketApi;
